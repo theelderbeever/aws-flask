4f0597263b876e8ef93d5130e92d556f39f031c8
Taylor Beever <taybeever@gmail.com>
2020-04-28 09:18:28 -0600

    factory init

---
+++ aws_flask/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+def create_app():
+    application = Flask(__name__, instance_relative_config=True,)
+
+    @application.route("/")
+    def index():
+        return "Hello factory __init__"
+
+    return application
--- aws_flask/application.py
+++ aws_flask/application.py
@@ -1,14 +1,6 @@
 from flask import Flask
 
-
-def create_app():
-    application = Flask(__name__, instance_relative_config=True,)
-
-    @application.route("/")
-    def index():
-        return "Hello dev"
-
-    return application
+from . import create_app
 
 
 application = create_app()
