c4823e4029ee4e4aab20f2647d46d9dd3cdd7e1b
Taylor Beever <taybeever@gmail.com>
2020-04-28 08:58:27 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="aws-flask",
+    version="0.1dev",
+    packages=["aws_flask"],
+    license="",
+    long_description=open("README.md").read(),
+)
